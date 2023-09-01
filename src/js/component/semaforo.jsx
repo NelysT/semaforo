054e877b4114d3811c2b45de48290c3fa7fdf200
NelysT <139475672+NelysT@users.noreply.github.com>
2023-09-01 23:46:40 +0000

    actualizando semaforo

---
+++ src/js/component/semaforo.jsx
@@ -0,0 +1,16 @@
+import React, { useState } from "react";
+
+const Semaforo = () => {
+    const [ color, setColor] = useState("red");
+
+    
+return (
+    <div className="semaforo">
+        <div onClick={()=> setColor("red")} className={"light red"+ (color === "red" ? "glow" : "")}></div>
+        <div onClick={()=> setColor("yellow")} className={"light yellow"+ (color === "yellow" ? "glow" : "")}></div>
+        <div onClick={()=> setColor("green")} className={"light green"+ (color === "green" ? "glow" : "")}></div>
+    </div>
+)
+
+}
+export default Semaforo;
