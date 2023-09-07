1d66af8a4816d7f975c561d14282cd9270cdd16d
NelysT <139475672+NelysT@users.noreply.github.com>
2023-09-07 17:43:04 +0000

    actualizando proyecto

--- src/js/component/semaforo.jsx
+++ src/js/component/semaforo.jsx
@@ -6,9 +6,9 @@ const Semaforo = () => {
     
 return (
     <div className="semaforo">
-        <div onClick={()=> setColor("red")} className={"light red"+ (color === "red" ? "glow" : "")}></div>
-        <div onClick={()=> setColor("yellow")} className={"light yellow"+ (color === "yellow" ? "glow" : "")}></div>
-        <div onClick={()=> setColor("green")} className={"light green"+ (color === "green" ? "glow" : "")}></div>
+        <div onClick={()=> setColor("red")} className={"light red" +(color === "red" ? " glow" : "")}></div>
+        <div onClick={()=> setColor("yellow")} className={"light yellow"+ (color === "yellow" ? " glow" : "")}></div>
+        <div onClick={()=> setColor("green")} className={"light green"+ (color === "green" ? " glow" : "")}></div>
     </div>
 )
 
